4eff75834c3915e47bf1bdd5e3ddb2a0e02a7fff
Saymon72 <semen66613@gmail.com>
2019-01-13 20:59:57 +0500

    note

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j.calculate;
 
+	/**
+	* Calculate.
+	*
+	* @author Semyon Sizov (semen66613@gmail.com)
+	* @version $Id$
+	* @since 0.1
+	*/
 public class Calculate {
 
    /**
@@ -7,8 +14,18 @@ public class Calculate {
     * @param args - args.
 	*/
 	
-	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		public static void main(String[] args) {
+			System.out.println("Hello, World");
 	}
+	
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+		public String echo(String name) {
+			return "Echo, echo, echo : " + name;
+	}
+ 
 	
 }
