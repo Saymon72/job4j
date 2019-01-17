cb54a72fea70d47d8abf7fa598d5312c7486e106
Saymon72 <semen66613@gmail.com>
2019-01-17 20:57:24 +0500

    3.1. Элементарный Калькулятор #103888

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,31 +1,30 @@
 package ru.job4j.calculate;
 
-	/**
-	* Calculate.
-	*
-	* @author Semyon Sizov (semen66613@gmail.com)
-	* @version $Id$
-	* @since 0.1
-	*/
+/**
+ * Calculate.
+ *
+ * @author Semyon Sizov (semen66613@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
 
-   /**
-	* Main.
-    * @param args - args.
-	*/
-	
-		public static void main(String[] args) {
-			System.out.println("Hello, World");
-	}
-	
 	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
-		public String echo(String name) {
-			return "Echo, echo, echo : " + name;
+	 * Main.
+	 * @param args - args.
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
 	}
- 
-	
+
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
+
 }
