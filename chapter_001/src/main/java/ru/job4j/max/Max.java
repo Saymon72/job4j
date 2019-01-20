a13252e3363b87e0a4c6a83f8015ad93506067b2
Saymon72 <semen66613@gmail.com>
2019-01-20 21:55:34 +0500

    4.2. Максимум из двух чисел[#103862]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,11 @@
+package ru.job4j.max;
+/**
+ * @author Semyon Sizov (semen66613@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    public int max(int first, int second){
+        return first < second ? first : second;
+    }
+}
