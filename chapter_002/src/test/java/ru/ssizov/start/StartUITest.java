70680980e1e0b429063dae34b61f75002d0143fb
Saymon72 <semen66613@gmail.com>
2019-03-05 20:18:43 +0500

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#103933]

--- chapter_002/src/test/java/ru/ssizov/start/StartUITest.java
+++ chapter_002/src/test/java/ru/ssizov/start/StartUITest.java
@@ -1,32 +1,45 @@
 package ru.ssizov.start;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.ssizov.models.Item;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void init() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(new PrintStream(stdout));
+
+    }
 
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
-        Tracker tracker = new Tracker();     // создаЄм Tracker
-        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});   //создаЄм StubInput с последовательностью действий
-        new StartUI(input, tracker).init();     //   создаЄм StartUI и вызываем метод init()
-        assertThat(tracker.findAll()[0].getName(), is("test name")); // провер€ем, что нулевой элемент массива в трекере содержит им€, введЄнное при эмул€ции.
+        Tracker tracker = new Tracker();
+        String[] action = {"0", "test name", "desc", "6"};
+        new StartUI(new StubInput(action), tracker).init();
+        assertThat(tracker.findAll()[0].getName(), is("test name"));
     }
 
     @Test
     public void whenUpdateThenTrackerHasUpdatedValue() {
-        // создаЄм Tracker
         Tracker tracker = new Tracker();
-        //Ќапр€мую добавл€ем за€вку
         Item item = tracker.add(new Item("test name", "desc"));
-        //создаЄм StubInput с последовательностью действий(производим замену за€вки)
-        Input input = new StubInput(new String[]{"2", item.getId(), "test replace", "заменили за€вку", "6"});
-        // создаЄм StartUI и вызываем метод init()
-        new StartUI(input, tracker).init();
-        // провер€ем, что нулевой элемент массива в трекере содержит им€, введЄнное при эмул€ции.
+        String[] action = {"2", item.getId(), "test replace", "заменили за€вку", "6"};
+        new StartUI(new StubInput(action), tracker).init();
         assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
     }
 
@@ -41,19 +54,26 @@ public class StartUITest {
 
     @Test
     public void whenUserFindIdItemThenTrackerHasNewItemWithSameName() {
-        Tracker tracker = new Tracker();     // создаЄм Tracker
+        Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("test name", "desc"));
-        Input input = new StubInput(new String[]{"0", "test name", "desc", "4", item.getId(), "6",});   //создаЄм StubInput с последовательностью действий
-        new StartUI(input, tracker).init();     //   создаЄм StartUI и вызываем метод init()
-        assertThat(tracker.findAll()[0].getName(), is("test name")); // провер€ем, что нулевой элемент массива в трекере содержит им€, введЄнное при эмул€ции.
+        String[] action = {"0", "test name", "desc", "4", item.getId(), "6",};
+        new StartUI(new StubInput(action), tracker).init();
+        assertThat(tracker.findAll()[0].getName(), is("test name"));
     }
 
     @Test
     public void whenUserFindNameItemThenTrackerHasNewItemWithSameName() {
-        Tracker tracker = new Tracker();     // создаЄм Tracker
-        //Item item = tracker.add(new Item("test name", "desc"));
-        Input input = new StubInput(new String[]{"0", "test name", "desc", "5", "test name", "6",});   //создаЄм StubInput с последовательностью действий
-        new StartUI(input, tracker).init();     //   создаЄм StartUI и вызываем метод init()
-        assertThat(tracker.findAll()[0].getName(), is("test name")); // провер€ем, что нулевой элемент массива в трекере содержит им€, введЄнное при эмул€ции.
+        Tracker tracker = new Tracker();
+        String[] action = {"0", "test name", "desc", "5", "test name", "6",};
+        new StartUI(new StubInput(action), tracker).init();
+        assertThat(tracker.findAll()[0].getName(), is("test name"));
+    }
+
+    @Test
+    public void WhenUserOutAllItems() {
+        Tracker tracker = new Tracker();
+        String[] action = {"0", "test name", "desc", "1", "6"};
+        new StartUI(new StubInput(action), tracker).init();
+        assertThat(tracker.findAll()[0].getName(), is("test name"));
     }
 }
